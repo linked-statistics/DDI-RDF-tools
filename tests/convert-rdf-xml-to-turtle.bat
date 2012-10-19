3fbb85857394daf6a701f36d138d718e94b6e9fa
Olof Olsson <borsna@gmail.com>
2012-10-19 13:53:12 +0200

    namespace fix and new DDI3.1 example

--- tests/convert-rdf-xml-to-turtle.bat
+++ tests/convert-rdf-xml-to-turtle.bat
@@ -3,3 +3,4 @@ if not exist rdf-ttl mkdir rdf-ttl
 rdfcopy rdf-xml/se.gu.snd.sims.ddi3.0801.rdf.xml RDF/XML TURTLE > rdf-ttl/se.gu.snd.sims.ddi3.0801.ttl
 rdfcopy rdf-xml/DDI2.1-generic.rdf.xml RDF/XML TURTLE > rdf-ttl/DDI2.1-generic.ttl
 rdfcopy rdf-xml/DDI3.1-generic.rdf.xml RDF/XML TURTLE > rdf-ttl/DDI3.1-generic.ttl
+rdfcopy rdf-xml/rdf-xml/DDI3.1-questionExample.rdf.xml RDF/XML TURTLE > rdf-ttl/rdf-xml/DDI3.1-questionExample.ttl
--- tests/generate-rdf-from-ddi-examples.bat
+++ tests/generate-rdf-from-ddi-examples.bat
@@ -1,6 +1,7 @@
 @echo off
 if not exist rdf-xml mkdir rdf-xml
 xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/se.gu.snd.sims.ddi3.0801.rdf.xml ../ddi-lifecycle/xsl/ddi3-1-to-rdf.xsl 	../ddi-examples/se.gu.snd.sims.ddi3.0801.xml 
-xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI3.1-generic.rdf.xml 			../ddi-lifecycle/xsl/ddi3-1-to-rdf.xsl 	../ddi-examples/DDI3.1-generic.xml
-xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI2.1-generic.rdf.xml 			../ddi-codebook/xsl/ddi2-1-to-rdf.xsl 	../ddi-examples/DDI2.1-generic.xml
+xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI3.1-generic.rdf.xml 		../ddi-lifecycle/xsl/ddi3-1-to-rdf.xsl 	../ddi-examples/DDI3.1-generic.xml
+xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI3.1-questionExample.rdf.xml 	../ddi-lifecycle/xsl/ddi3-1-to-rdf.xsl 	../ddi-examples/DDI3.1-questionExample.xml
+xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI2.1-generic.rdf.xml 		../ddi-codebook/xsl/ddi2-1-to-rdf.xsl 	../ddi-examples/DDI2.1-generic.xml
 
