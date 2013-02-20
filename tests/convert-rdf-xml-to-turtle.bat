6e1aaa8e4315d9dd9eb09027a304dc9eb25250bc
Olof Olsson <borsna@gmail.com>
2013-02-20 14:14:16 +0100

    merged conversion-script

--- tests/convert-rdf-xml-to-turtle.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-if not exist rdf-ttl mkdir rdf-ttl
-rdfcopy rdf-xml/se.gu.snd.sims.ddi3.0801.rdf.xml RDF/XML TURTLE > rdf-ttl/se.gu.snd.sims.ddi3.0801.ttl
-rdfcopy rdf-xml/DDI2.1-generic.rdf.xml RDF/XML TURTLE > rdf-ttl/DDI2.1-generic.ttl
-rdfcopy rdf-xml/DDI3.1-generic.rdf.xml RDF/XML TURTLE > rdf-ttl/DDI3.1-generic.ttl
-rdfcopy rdf-xml/rdf-xml/DDI3.1-questionExample.rdf.xml RDF/XML TURTLE > rdf-ttl/rdf-xml/DDI3.1-questionExample.ttl
--- tests/generate-rdf-from-ddi-examples.bat
+++ tests/generate-rdf-from-ddi-examples.bat
@@ -1,5 +1,8 @@
-@echo off
+
 if not exist rdf-xml mkdir rdf-xml
 if not exist rdf-ttl mkdir rdf-ttl
-xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI3.1-generic.rdf.xml 		../xslt/ddi-to-rdf.xsl          ../ddi-examples/DDI3.1-generic.xml
+
+xsltproc --stringparam studyURI http://my.prefix.com/study/42 --output rdf-xml/DDI3.1-generic.rdf.xml ../xslt/ddi-to-rdf.xsl ../ddi-examples/DDI3.1-generic.xml
+xsltproc --stringparam studyURI http://snd.gu.se/stsasd --output rdf-xml/se.gu.snd.sims.ddi3.0856.rdf.xml ../xslt/ddi-to-rdf.xsl ../ddi-examples/se.gu.snd.sims.ddi3.0856.xml
+rdfcopy rdf-xml/se.gu.snd.sims.ddi3.0856.rdf.xml RDF/XML TURTLE > rdf-ttl/se.gu.snd.sims.ddi3.0856.ttl
 rdfcopy rdf-xml/DDI3.1-generic.rdf.xml RDF/XML TURTLE > rdf-ttl/DDI3.1-generic.ttl
