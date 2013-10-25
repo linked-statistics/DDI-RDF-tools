0943121317e97f974cc45cae126bd00971123926
Olof Olsson <borsna@gmail.com>
2013-10-25 15:41:15 +0200

    removed crap

--- tests/run-sparql-examples.bat
+++ tests/run-sparql-examples.bat
@@ -1,5 +1,5 @@
 @echo off
 echo 
-echo Find questions containing the letter a
+
 for %%x in (sparql/*.rq) do sparql --data=rdf-ttl/ISSP.1994.Family-and-changing-gender-roles-II-Sweden.ttl --query=sparql/%%x
 echo 
